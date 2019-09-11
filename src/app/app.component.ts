883e70ffc555f454eb349891ec1921db4605213f
Daniel da Silva de Lima <dsl15021996@gmail.com>
2019-09-11 18:17:08 -0300

    adicionado items ao menus, porém falta colocar no arquivo de routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,20 @@ import { PoMenuItem } from '@portinari/portinari-ui';
 export class AppComponent {
 
   readonly menus: Array<PoMenuItem> = [
-    { label: 'Cadastros', shortLabel: 'Cadastros', link: '/cadastros', icon: 'po-icon po-icon-document-filled' },
-    { label: 'Chamados', shortLabel: 'Chamados', link: '/chamados', icon: 'po-icon po-icon-list' },
-    { label: 'Settings', shortLabel: 'Settings', link: '/settings', icon: 'po-icon po-icon-settings' },
+    {
+      label: 'Cadastros', shortLabel: 'Cadastros', icon: 'po-icon po-icon-document-filled',
+      subItems: [
+        { label: 'Analistas', shortLabel: 'Analistas', link: 'cadastros/analista-list', icon: 'po-icon po-icon-document-filled' },
+        { label: 'Empresas', shortLabel: 'Empresas', link: 'cadastros/empresa-list', icon: 'po-icon po-icon-document-filled' },
+      ]
+    },
+    { label: 'Chamados', shortLabel: 'Chamados', link: 'chamados/chamados-list', icon: 'po-icon po-icon-list' },
+    {
+      label: 'Configurações', shortLabel: 'Configurações', icon: 'po-icon po-icon-settings', subItems: [
+        { label: 'Usuários', shortLabel: 'Usuários', link: 'settings/user-list', icon: 'po-icon po-icon-users' },
+        { label: 'Regras', shortLabel: 'Regras', link: 'settings/role-list', icon: 'po-icon po-icon-minus' },
+      ]
+    },
     // { label: '', shortLabel: '', link: '', icon: '' },
   ];
 
--- src/app/chamados/chamados-routing.module.ts
+++ src/app/chamados/chamados-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChamadosListComponent } from './chamados-list/chamados-list.component';
+import { ChamadosEditComponent } from './chamados-edit/chamados-edit.component';
+import { ChamadosAddComponent } from './chamados-add/chamados-add.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChamadosListComponent },
+  { path: 'edit/:id', component: ChamadosEditComponent },
+  { path: 'view/:id', component: ChamadosEditComponent },
+  { path: 'add', component: ChamadosAddComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
