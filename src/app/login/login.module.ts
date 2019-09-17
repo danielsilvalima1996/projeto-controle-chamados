4f40b38083904e29b98d6aba6912215692041f30
Daniel da Silva de Lima <dsl15021996@gmail.com>
2019-09-17 18:20:57 -0300

    finalizado login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
+import { PoFieldModule, PoPageModule, PoButtonModule } from '@portinari/portinari-ui';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    PoPageModule,
+    PoFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PoButtonModule
   ]
 })
 export class LoginModule { }
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup = this.fb.group({
+    email: ['', []],
+    password: ['', []]
+  })
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
   }
