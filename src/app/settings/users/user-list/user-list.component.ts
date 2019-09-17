88f45fbd269d5efbc9c70fbda1cfc26dedc180c4
aksilvaa <alison.keuver@gmail.com>
2019-09-16 23:16:47 -0300

    cliente-list wip

--- src/app/settings/users/user-list/user-list.component.ts
+++ src/app/settings/users/user-list/user-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { PoPageDefault } from '@portinari/portinari-ui';
+import { PoPageDefault, PoSelectOption, PoTableColumn, PoTableAction, PoPageAction, PoBreadcrumb, PoBreadcrumbItem } from '@portinari/portinari-ui';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { UserListService } from 'src/app/services/users/user-list.service';
 
 @Component({
   selector: 'app-user-list',
@@ -8,18 +10,73 @@ import { PoPageDefault } from '@portinari/portinari-ui';
 })
 export class UserListComponent implements OnInit {
 
-  page:  PoPageDefault = {
+  page = {
+    actions: <PoPageAction[]>[
+      { label: 'Novo', icon: 'po-icon po-icon-user-add', url: 'user-list/add' }
+    ],
 
-    title:'Usuários',
-    
-
-
-
+    title: 'Cadastro de Usuários',
+    breadcrumb: <PoBreadcrumb>{
+      items: <PoBreadcrumbItem[]>[
+        { label: 'Home' },
+        { label: 'Configurações' },
+        { label: 'Usuários' }
+      ]
+    }
   }
 
-  constructor() { }
+  table = {
+    columns: <PoTableColumn[]>[
+      { property: 'idUser', label: 'ID', width: '10%' },
+      { property: 'nomeUser', label: 'Nome', width: '15%' },
+      { property: 'emailUser', label: 'E-mail', width: '25%' },
+      { property: 'idEmpresa', label: 'ID Empresa', width: '10%' },
+      { property: 'regra', label: 'Regra', width: '10%' },
+      { property: 'created', label: 'Criado ', width: '10%', type: 'date', format: 'dd/MM/yyyy' },
+      { property: 'modified', label: 'Modificado ', width: '10%', type: 'date', format: 'dd/MM/yyyy' },
+      { property: 'status', label: 'Status', width: '10%' }
+    ],
+    actions: <PoTableAction[]>[
+      { label: 'Visualizar', url: 'user-list/view:id' },
+      { label: 'Editar', url: 'user-list/edit:id' },
+    ],
+    items: [],
+    height: 0,
+    loading: false
+  }
+
+  userform: FormGroup = this.fb.group({
+    filtro: ['', [Validators.required]],
+    pesquisa: ['']
+  })
+
+  selects = {
+    pesquisa: <PoSelectOption[]>[
+      { label: 'ID', value: 'id' },
+      { label: 'USUÁRIO', value: 'user' },
+      { label: 'E-MAIL', value: 'email' },
+      { label: 'Regra', value: 'regra' },
+      { label: 'STATUS', value: 'status' }
+    ]
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserListService
+  ) { }
+
+
 
   ngOnInit() {
+    this.getUser()
+  }
+
+  private getUser() {
+    this.userService.getUser()
+      .subscribe((data) => {
+        this.table.items = data
+      })
+
   }
 
 }
