d4a67afc95d7cad8d0dd4bee299bd50fc934ffeb
Daniel da Silva de Lima <dsl15021996@gmail.com>
2019-10-16 20:13:56 -0300

    wip subtipo chamado

--- src/app/cadastros/subtipo-chamado/subtipo-chamado-list/subtipo-chamado-list.component.ts
+++ src/app/cadastros/subtipo-chamado/subtipo-chamado-list/subtipo-chamado-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { PoPageDefault, PoTableColumn, PoSelectOption } from '@portinari/portinari-ui';
+import { SubtipoChamadoService } from 'src/app/services/chamados/subtipo-chamado/subtipo-chamado.service';
+import { UtilService } from 'src/app/services/utils/util-service/util.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-subtipo-chamado-list',
@@ -7,9 +12,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubtipoChamadoListComponent implements OnInit {
 
-  constructor() { }
+  page: PoPageDefault = {
+    title: 'Lista de SubTipo de Chamados',
+    breadcrumb: {
+      items: [
+        { label: 'Cadastros' },
+        { label: 'SubTipo de Chamados' },
+      ]
+    },
+    actions: [
+      { label: 'Novo', url: 'subtipo-chamado/add' },
+      {
+        label: 'Editar', action: () => {
+
+          //fazer uma funcao
+          this.router.navigate(['edit', this.constValue.selecionado], { relativeTo: this.route });
+        }
+      }
+    ],
+  }
+
+  table = {
+    columns: <PoTableColumn[]>[
+      { property: 'id', label: 'ID', width: '10%' },
+      { property: 'descricao', label: 'Descrição', width: '20%' },
+      { property: 'active', label: 'Ativo', width: '10%', type: 'boolean' },
+      { property: 'idTipoChamado', label: 'Id Tipo Chamado', width: '10%' },
+      { property: 'created', label: 'Criado', width: '20%', type: 'date', format: 'dd/MM/yyyy' },
+      { property: 'modified', label: 'Modificado', width: '20%', type: 'date', format: 'dd/MM/yyyy' }
+    ],
+    items: [],
+    height: 0,
+    loading: false
+  }
+
+  selects = {
+    pesquisa: <PoSelectOption[]>[
+      { label: 'ID', value: 'id' },
+      { label: 'DESCRIÇÃO', value: 'descricao' },
+      { label: 'ATIVO', value: 'active' },
+      {label: 'Id Tipo Chamado', value: 'idTipoChamado'}
+    ],
+    filtro: <PoSelectOption[]>[
+      { label: 'SIM', value: 'true' },
+      { label: 'NÃO', value: 'false' }
+    ]
+  }
+
+  constValue = {
+    selecionado: ''
+  }
+
+  subtipoForm: FormGroup = this.fb.group({
+    filtro: ['', []],
+    pesquisa: ['', []]
+  })
+
+  constructor(
+    private subtipoChamadoService: SubtipoChamadoService,
+    private utilService: UtilService,
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+  }
+
+  findSubtipoChamado(parameters: any) {
+    this.subtipoChamadoService
+      .findSubtipoChamado(this.utilService
+        .getParameters(parameters))
+      .subscribe((data) => {
+        console.log(data);
+      })
   }
 
 }
--- src/app/services/chamados/subtipo-chamado/subtipo-chamado.service.ts
+++ src/app/services/chamados/subtipo-chamado/subtipo-chamado.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SubtipoChamado } from 'src/app/interfaces/subtipo-chamado.model';
+import { Pageable } from 'src/app/interfaces/pageable.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubtipoChamadoService {
 
-  constructor() { }
+  private url = `${environment.url.apirest}/subtipo-chamado`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  findSubtipoChamado(parameters: any): Observable<Pageable<SubtipoChamado>> {
+    return this.http.get(`${this.url}?${parameters}`) as Observable<Pageable<SubtipoChamado>>;
+  }
+
+  findById(id: number): Observable<SubtipoChamado> {
+    return this.http.get(`${this.url}/${id}`) as Observable<SubtipoChamado>;
+  }
 }
