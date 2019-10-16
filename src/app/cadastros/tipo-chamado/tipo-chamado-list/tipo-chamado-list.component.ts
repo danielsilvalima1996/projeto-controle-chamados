7d9277ed0aca4f13bde677a7d3f2ab493fdf72b7
aksilvaa <alison.keuver@gmail.com>
2019-10-16 20:12:00 -0300

    tipo-chamado list wip

--- src/app/cadastros/tipo-chamado/tipo-chamado-list/tipo-chamado-list.component.ts
+++ src/app/cadastros/tipo-chamado/tipo-chamado-list/tipo-chamado-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { PoPageAction, PoBreadcrumb, PoBreadcrumbItem, PoTableColumn, PoSelectOption } from '@portinari/portinari-ui';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { TipoChamadoService } from 'src/app/services/chamados/tipo-chamado/tipo-chamado.service';
+import { UtilService } from 'src/app/services/utils/util-service/util.service';
 
 @Component({
   selector: 'app-tipo-chamado-list',
@@ -7,9 +12,98 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TipoChamadoListComponent implements OnInit {
 
-  constructor() { }
+  page = {
+    actions: <PoPageAction[]>[
+      { label: 'Novo', url: 'tipo-chamado/add' },
+      { label: 'Editar', action: () => {this.router.navigate(['edit', this.constValue.selecionado], {relativeTo:this.route})}}
+    ],
+
+    title: 'Tipos de Chamado',
+    breadcrumb: <PoBreadcrumb>{
+      items: <PoBreadcrumbItem[]>[
+        { label: 'Home' },
+        { label: 'Cadastros' },
+        { label: 'Tipos Chamado' }
+      ]
+    }
+  }
+
+  table = {
+    columns: <PoTableColumn[]>[
+      { property: 'id', label: 'ID', width: '10%' },
+      { property: 'descricao', label: 'Descrição', width: '30%' },
+      { property: 'created', label: 'Criado ', width: '10%', type: 'date', format: 'dd/MM/yyyy' },
+      { property: 'modified', label: 'Modificado ', width: '10%', type: 'date', format: 'dd/MM/yyyy' },
+      { property: 'active', label: 'Ativo', width: '10%', type:'boolean' }
+    ],
+    items: [],
+    height: 0,
+    loading: false
+  }
+
+  selects = {
+    pesquisa: <PoSelectOption[]>[
+      { label: 'ID', value: 'id' },
+      { label: 'DESCRIÇÃO', value: 'descricao' },
+      { label: 'ATIVO', value: 'active' },
+    ],
+    filtro: <PoSelectOption[]>[
+      { label: 'SIM', value: 'true' },
+      { label: 'NÃO', value: 'false' }
+    ]
+  }
+
+  tipoChamadoForm: FormGroup = this.fb.group({
+    filtro: ['', [Validators.required]],
+    pesquisa: ['']
+  })
+
+  constValue = {
+    selecionado:'',
+    input: <Boolean>true,
+    select:<Boolean>false,
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private tipoChamadoService: TipoChamadoService,
+    private utilService: UtilService
+  ) { }
 
   ngOnInit() {
+    this.controls.pesquisa
+    .valueChanges.subscribe((data) => {
+      this.tipoForm(data);
+    })
+    this.getTipoChamado(this.tipoChamadoForm.value);
+  }
+
+  get controls() {
+    return this.tipoChamadoForm.controls;
+  }
+
+  tipoForm(tipo) {
+    if (tipo == 'active') {
+      this.constValue.input = false;
+      this.constValue.select = true;
+    } else {
+      this.constValue.input = true;
+      this.constValue.select = false;
+    }
+  }
+
+   getTipoChamado(form?) {
+    this.tipoChamadoService.getTipoChamado(this.utilService.getParameters(form))
+      .subscribe((data:any) => {
+        this.table.items = data.content
+      })
+  }
+
+  getSelected(event) {
+    this.constValue.selecionado = event.id;
+    
   }
 
 }
--- src/app/services/chamados/tipo-chamado/tipo-chamado.service.ts
+++ src/app/services/chamados/tipo-chamado/tipo-chamado.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { TipoChamado } from 'src/app/interfaces/tipo-chamado.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +10,14 @@ import { Injectable } from '@angular/core';
 
 export class TipoChamadoService {
 
-  constructor() { }
+  private relativeLink = 'tipo-chamado'
+  private url = `${environment.url.apirest}${this.relativeLink}`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getTipoChamado(parameters:any): Observable<TipoChamado> {
+    return this.http.get(`${environment.url.apirest}/${this.relativeLink}?${parameters}`) as Observable<TipoChamado> ;
+  }
 }
