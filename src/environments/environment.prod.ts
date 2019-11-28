c3e36e68b8e5d297afc4ab32e281a550987e0478
danielsilvalima1996 <dsl15021996@gmail.com>
2019-11-27 22:29:18 -0300

    version 0.0.36

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   url: {
-    apirest: 'http://localhost:5000/api'
+    // apirest: 'http://localhost:5000/api'
+    apirest: 'http://18.229.143.88:5000/api'
   }
 };
