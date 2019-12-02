71d34563411c57b01d5d62ee2616d4d78c254dee
dlima <daniel.lima@ogrupogc.com.br>
2019-12-02 10:39:04 -0300

    version 0.0.38

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   url: {
     // apirest: 'http://localhost:5000/api'
-    apirest: 'http://ec2-18-229-133-182.sa-east-1.compute.amazonaws.com:5000/api'
+    apirest: 'http://192.168.100.59:5000/api'
   }
 };
