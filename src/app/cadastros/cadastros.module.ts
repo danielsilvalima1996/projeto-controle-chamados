422ffb549cdf77f1fefd6bd9d93c8c9d4057eefd
danielsilvalima1996 <dsl15021996@gmail.com>
2020-10-14 20:29:33 -0300

    removido arquivos desnecessários

--- src/app/cadastros/cadastros.module.ts
+++ src/app/cadastros/cadastros.module.ts
@@ -1,38 +1,29 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PoAccordionModule, PoAvatarModule, PoButtonModule, PoDividerModule, PoDynamicModule, PoFieldModule, PoLoadingModule, PoModalModule, PoPageModule, PoTableModule } from '@po-ui/ng-components';
+import { SharedModule } from '../shared/shared.module';
 import { CadastrosRoutingModule } from './cadastros-routing.module';
-import { AnalistaEditComponent } from './analist/analista-edit/analista-edit.component';
-import { AnalistaAddComponent } from './analist/analista-add/analista-add.component';
-import { AnalistaListComponent } from './analist/analista-list/analista-list.component';
-import { PoPageModule, PoButtonModule, PoTableModule, PoFieldModule, PoLoadingModule, PoDividerModule, PoModalModule, PoDynamicModule, PoAccordionModule, PoAvatarModule } from '@po-ui/ng-components';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { UserEditComponent } from './users/user-edit/user-edit.component';
-import { UserAddComponent } from './users/user-add/user-add.component';
-import { UserListComponent } from './users/user-list/user-list.component';
-import { TipoChamadoListComponent } from './tipo-chamado/tipo-chamado-list/tipo-chamado-list.component';
-import { TipoChamadoAddComponent } from './tipo-chamado/tipo-chamado-add/tipo-chamado-add.component';
-import { TipoChamadoEditComponent } from './tipo-chamado/tipo-chamado-edit/tipo-chamado-edit.component';
-import { SubtipoChamadoAddComponent } from './subtipo-chamado/subtipo-chamado-add/subtipo-chamado-add.component';
-import { SubtipoChamadoListComponent } from './subtipo-chamado/subtipo-chamado-list/subtipo-chamado-list.component';
-import { SubtipoChamadoEditComponent } from './subtipo-chamado/subtipo-chamado-edit/subtipo-chamado-edit.component';
-import { PermissionsListComponent } from './permissions/permissions-list/permissions-list.component';
-import { PermissionsAddComponent } from './permissions/permissions-add/permissions-add.component';
-import { PermissionsEditComponent } from './permissions/permissions-edit/permissions-edit.component';
-import { EmpresaListComponent } from './empresa/empresa-list/empresa-list.component';
 import { EmpresaAddComponent } from './empresa/empresa-add/empresa-add.component';
 import { EmpresaEditComponent } from './empresa/empresa-edit/empresa-edit.component';
-import { SharedModule } from '../shared/shared.module';
-import { RegrasListComponent } from './regras/regras-list/regras-list.component';
+import { EmpresaListComponent } from './empresa/empresa-list/empresa-list.component';
 import { RegrasEditComponent } from './regras/regras-edit/regras-edit.component';
-import { TecnicosListComponent } from './tecnicos/tecnicos-list/tecnicos-list.component';
+import { RegrasListComponent } from './regras/regras-list/regras-list.component';
+import { SubtipoChamadoAddComponent } from './subtipo-chamado/subtipo-chamado-add/subtipo-chamado-add.component';
+import { SubtipoChamadoEditComponent } from './subtipo-chamado/subtipo-chamado-edit/subtipo-chamado-edit.component';
+import { SubtipoChamadoListComponent } from './subtipo-chamado/subtipo-chamado-list/subtipo-chamado-list.component';
 import { TecnicosEditComponent } from './tecnicos/tecnicos-edit/tecnicos-edit.component';
+import { TecnicosListComponent } from './tecnicos/tecnicos-list/tecnicos-list.component';
+import { TipoChamadoAddComponent } from './tipo-chamado/tipo-chamado-add/tipo-chamado-add.component';
+import { TipoChamadoEditComponent } from './tipo-chamado/tipo-chamado-edit/tipo-chamado-edit.component';
+import { TipoChamadoListComponent } from './tipo-chamado/tipo-chamado-list/tipo-chamado-list.component';
+import { UserAddComponent } from './users/user-add/user-add.component';
+import { UserEditComponent } from './users/user-edit/user-edit.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+
 
 @NgModule({
   declarations: [
-    AnalistaEditComponent,
-    AnalistaAddComponent,
-    AnalistaListComponent,
     UserEditComponent,
     UserAddComponent,
     UserListComponent,
@@ -42,9 +33,6 @@ import { TecnicosEditComponent } from './tecnicos/tecnicos-edit/tecnicos-edit.co
     SubtipoChamadoAddComponent,
     SubtipoChamadoListComponent,
     SubtipoChamadoEditComponent,
-    PermissionsListComponent,
-    PermissionsAddComponent,
-    PermissionsEditComponent,
     EmpresaListComponent,
     EmpresaAddComponent,
     EmpresaEditComponent,
